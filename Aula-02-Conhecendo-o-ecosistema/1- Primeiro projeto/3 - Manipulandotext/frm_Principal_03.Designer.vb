c141b8f3ae333fa80eb123edb20cc9583a37647c
Daniel Sabino <dannisabino17@gmail.com>
2023-07-26 23:48:28 -0300

    Manipulando textos

--- Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/3 - Manipulandotext/frm_Principal_03.Designer.vb
+++ Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/3 - Manipulandotext/frm_Principal_03.Designer.vb
@@ -24,13 +24,18 @@ Partial Class frm_Principal_03
     Private Sub InitializeComponent()
         Me.Btm_Principal = New System.Windows.Forms.Button()
         Me.lbl_NomeProjeto = New System.Windows.Forms.Label()
+        Me.lbl_Curso = New System.Windows.Forms.Label()
+        Me.txt_curso = New System.Windows.Forms.TextBox()
+        Me.btm_adicionar = New System.Windows.Forms.Button()
+        Me.txt_lista = New System.Windows.Forms.TextBox()
+        Me.btm_MostraLista = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Btm_Principal
         '
         Me.Btm_Principal.Location = New System.Drawing.Point(12, 29)
         Me.Btm_Principal.Name = "Btm_Principal"
-        Me.Btm_Principal.Size = New System.Drawing.Size(125, 125)
+        Me.Btm_Principal.Size = New System.Drawing.Size(458, 37)
         Me.Btm_Principal.TabIndex = 0
         Me.Btm_Principal.Text = "Clique aqui"
         Me.Btm_Principal.UseVisualStyleBackColor = True
@@ -44,11 +49,59 @@ Partial Class frm_Principal_03
         Me.lbl_NomeProjeto.TabIndex = 1
         Me.lbl_NomeProjeto.Text = "1 - Primeiro projeto"
         '
+        'lbl_Curso
+        '
+        Me.lbl_Curso.AutoSize = True
+        Me.lbl_Curso.Location = New System.Drawing.Point(12, 81)
+        Me.lbl_Curso.Name = "lbl_Curso"
+        Me.lbl_Curso.Size = New System.Drawing.Size(51, 17)
+        Me.lbl_Curso.TabIndex = 2
+        Me.lbl_Curso.Text = "Label1"
+        '
+        'txt_curso
+        '
+        Me.txt_curso.Location = New System.Drawing.Point(15, 101)
+        Me.txt_curso.Name = "txt_curso"
+        Me.txt_curso.Size = New System.Drawing.Size(209, 22)
+        Me.txt_curso.TabIndex = 3
+        '
+        'btm_adicionar
+        '
+        Me.btm_adicionar.Location = New System.Drawing.Point(230, 81)
+        Me.btm_adicionar.Name = "btm_adicionar"
+        Me.btm_adicionar.Size = New System.Drawing.Size(240, 42)
+        Me.btm_adicionar.TabIndex = 4
+        Me.btm_adicionar.Text = "Clique aqui"
+        Me.btm_adicionar.UseVisualStyleBackColor = True
+        '
+        'txt_lista
+        '
+        Me.txt_lista.Location = New System.Drawing.Point(15, 129)
+        Me.txt_lista.Multiline = True
+        Me.txt_lista.Name = "txt_lista"
+        Me.txt_lista.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txt_lista.Size = New System.Drawing.Size(455, 210)
+        Me.txt_lista.TabIndex = 5
+        '
+        'btm_MostraLista
+        '
+        Me.btm_MostraLista.Location = New System.Drawing.Point(15, 345)
+        Me.btm_MostraLista.Name = "btm_MostraLista"
+        Me.btm_MostraLista.Size = New System.Drawing.Size(455, 42)
+        Me.btm_MostraLista.TabIndex = 6
+        Me.btm_MostraLista.Text = "Clique aqui"
+        Me.btm_MostraLista.UseVisualStyleBackColor = True
+        '
         'frm_Principal_03
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(482, 453)
+        Me.Controls.Add(Me.btm_MostraLista)
+        Me.Controls.Add(Me.txt_lista)
+        Me.Controls.Add(Me.btm_adicionar)
+        Me.Controls.Add(Me.txt_curso)
+        Me.Controls.Add(Me.lbl_Curso)
         Me.Controls.Add(Me.lbl_NomeProjeto)
         Me.Controls.Add(Me.Btm_Principal)
         Me.Name = "frm_Principal_03"
@@ -61,4 +114,9 @@ Partial Class frm_Principal_03
 
     Friend WithEvents Btm_Principal As Button
     Friend WithEvents lbl_NomeProjeto As Label
+    Friend WithEvents lbl_Curso As Label
+    Friend WithEvents txt_curso As TextBox
+    Friend WithEvents btm_adicionar As Button
+    Friend WithEvents txt_lista As TextBox
+    Friend WithEvents btm_MostraLista As Button
 End Class
--- Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/3 - Manipulandotext/frm_Principal_03.vb
+++ Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/3 - Manipulandotext/frm_Principal_03.vb
@@ -1,4 +1,7 @@
 ﻿Public Class frm_Principal_03
+
+    Dim ListaCurso As String
+
     Public Sub New()
 
         ' Esta chamada é requerida pelo designer.
@@ -8,5 +11,19 @@
         lbl_NomeProjeto.Text = "3 - Manipulando Texto"
         Btm_Principal.Text = "Clique para ver os resultados"
         Me.Text = "3 - Manipulando Texto"
+        lbl_Curso.Text = "Digite o seu curso favorito"
+        btm_adicionar.Text = "Adicione a sua lista"
+        btm_MostraLista.Text = "Mostra lista atual"
+    End Sub
+
+    Private Sub btm_adicionar_Click(sender As Object, e As EventArgs) Handles btm_adicionar.Click
+        Dim CursoDigitado As String = txt_curso.Text
+        ListaCurso = ListaCurso + CursoDigitado + vbCrLf
+        txt_lista.Text = ListaCurso
+        txt_curso.Text = ""
+    End Sub
+
+    Private Sub btm_MostraLista_Click(sender As Object, e As EventArgs) Handles btm_MostraLista.Click
+        MsgBox(ListaCurso)
     End Sub
 End Class
