dfd60dd172ab3e63a6327eea7cb37946fa340a42
Daniel Sabino <dannisabino17@gmail.com>
2023-08-02 21:37:21 -0300

    Criando novo projeto

---
+++ Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/4 - Controledefluxo/Frm_Principal_04.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal_04
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btm_Principal = New System.Windows.Forms.Button()
+        Me.lbl_NomeProjeto = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Btm_Principal
+        '
+        Me.Btm_Principal.Location = New System.Drawing.Point(12, 29)
+        Me.Btm_Principal.Name = "Btm_Principal"
+        Me.Btm_Principal.Size = New System.Drawing.Size(125, 125)
+        Me.Btm_Principal.TabIndex = 0
+        Me.Btm_Principal.Text = "Clique aqui"
+        Me.Btm_Principal.UseVisualStyleBackColor = True
+        '
+        'lbl_NomeProjeto
+        '
+        Me.lbl_NomeProjeto.AutoSize = True
+        Me.lbl_NomeProjeto.Location = New System.Drawing.Point(12, 9)
+        Me.lbl_NomeProjeto.Name = "lbl_NomeProjeto"
+        Me.lbl_NomeProjeto.Size = New System.Drawing.Size(129, 17)
+        Me.lbl_NomeProjeto.TabIndex = 1
+        Me.lbl_NomeProjeto.Text = "1 - Primeiro projeto"
+        '
+        'Frm_Principal_04
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 453)
+        Me.Controls.Add(Me.lbl_NomeProjeto)
+        Me.Controls.Add(Me.Btm_Principal)
+        Me.Name = "Frm_Principal_04"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplicação - Ola Mundo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btm_Principal As Button
+    Friend WithEvents lbl_NomeProjeto As Label
+End Class
---
+++ Aula-02-Conhecendo-o-ecosistema/1- Primeiro projeto/4 - Controledefluxo/Frm_Principal_04.vb
@@ -0,0 +1,13 @@
+﻿Public Class Frm_Principal_04
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+        lbl_NomeProjeto.Text = "4 - Controle de fluxo"
+        Btm_Principal.Text = "Clique para ver os resultados"
+        Me.Text = "4 - Controle de fluxo"
+
+    End Sub
+End Class
